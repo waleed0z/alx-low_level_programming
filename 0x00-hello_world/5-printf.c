c8dcf4f686130a3966b9185e51466dc2e76b5ff7
waleed0z <waleedashir6@gmail.com>
2022-09-11 04:24:18 -0700

    betty-style

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+
+/**
+ * main - prints with proper grammar,
+ *
+ * but the outcome is a piece of art,
+ *
+ * Return: 0 if exit properly.
+ *
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}

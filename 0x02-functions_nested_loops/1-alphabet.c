8b120e1689edd9b34c12c4c1972aef9719f4293f
waleed0z <waleedashir6@gmail.com>
2022-09-14 18:55:23 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,8 @@
 
 
 /**
- *
- *  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
- *
- *   */
+* print_alphabet - prints the alphabet, in lowercase, followed by a new line
+*/
 
 void print_alphabet(void)
 

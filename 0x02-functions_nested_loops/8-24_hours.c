a57696fd22e09c0da059f59af9ac8c8c516796f8
waleed0z <waleedashir6@gmail.com>
2022-09-17 17:59:12 -0700

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - void jack_bauer(void) function writes every minute of
+ * hh:mm period
+ * @void: void
+ * Return: 0
+*/
+
+void jack_bauer(void)
+
+{
+		int ho, mi;
+
+	for (ho = 00; ho < 24; ho++)
+	{
+		for (mi = 00; mi < 60; mi++)
+		{
+				if (ho < 24 && mi < 60)
+			{
+				_putchar((ho / 10) + '0');
+				_putchar((ho % 10) + '0');
+				_putchar(':');
+				_putchar((mi / 10) + '0');
+				_putchar((mi % 10) + '0');
+				_putchar('\n');
+			}
+		}
+	}
+/*	return (0);*/
+}
